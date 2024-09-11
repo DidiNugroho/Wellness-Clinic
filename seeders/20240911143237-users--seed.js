d299f7f9f7fa956ac1abe11e97f32ebcbbcd0945
is2mael <ismael.muh26@gmail.com>
2024-09-11 21:33:28 +0700

    lupa pull

---
+++ seeders/20240911143237-users--seed.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('People', [{
+      name: 'John Doe',
+      isBetaMember: false
+    }], {});
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+    */
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('People', null, {});
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     */
+  }
+};
