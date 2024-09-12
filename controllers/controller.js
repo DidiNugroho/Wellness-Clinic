f7ed6e4f7d0ac6d7e90ea7f95b7039910fd05a44
is2mael <ismael.muh26@gmail.com>
2024-09-12 12:26:15 +0700

    register

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,5 +1,6 @@
+const { Op } = require('sequelize');
 const { User } = require('../models')
-
+const bcrypt = require('bcryptjs')
 module.exports.home = async (req, res) => {  
     try {  
         res.render('Home')
@@ -17,9 +18,38 @@ module.exports.getRegister = async (req, res) => {
 };
 
 module.exports.postRegister = async (req, res) => {  
+    let { name, email, password, role } = req.body
     try {  
-        const hashedPassword = 
-        res.redirect('/')
+        let check = await User.findOne({
+            where: {
+                email: {
+                    [Op.iLike]: `${email}`
+                }
+            }
+        });
+        if (check) {
+            res.redirect('/register?error=email sudah terpakai')
+        } else {
+            let newUser = await User.create({
+                name, email, password, role
+            });
+            if (newUser) {
+                req.session.userId = newUser.id;
+                req.session.role = newUser.role;
+                res.redirect('/login')
+            } else {
+                res.redirect('/register?error=register gagal')
+            }
+        }
+
+    } catch (error) {  
+        res.send(error.message)
+    }  
+};
+
+module.exports.getLogin = async (req, res) => {  
+    try {  
+        res.render('Login')
     } catch (error) {  
         res.send(error.message)
     }  
--- routers/index.js
+++ routers/index.js
@@ -4,5 +4,6 @@ const { home, getRegister, postRegister } = require('../controllers/controller')
 router.get('/', home)
 router.get('/register', getRegister)
 router.post('/register', postRegister)
+router.get('/login', )
 
 module.exports = router;
