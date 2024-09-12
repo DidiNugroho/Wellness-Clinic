128f0e345dbc21aca7a1a9251877c8998181210b
RenFrost <diidnugroho@gmail.com>
2024-09-12 13:47:40 +0700

    tambah fitur

--- routers/index.js
+++ routers/index.js
@@ -1,8 +1,16 @@
 const router = require('express').Router()
-const { home, getRegister, postRegister } = require('../controllers/controller')
+const { home, getRegister, postRegister, readDoctors, readPatients, readIllnesses, getAddIllness, postAddIllness } = require('../controllers/controller')
 
 router.get('/', home)
 router.get('/register', getRegister)
 router.post('/register', postRegister)
+router.get('/doctors', readDoctors)
+router.get('/patients', readPatients)
+router.get('/illnesses', readIllnesses)
+router.get('/illnesses/add', getAddIllness)
+router.post('/illnesses/add', postAddIllness)
+router.get('/illnesses/:id/edit')
+router.post('/illnesses/:id/edit')
+router.get('/illnesses/:id/delete')
 
 module.exports = router;
